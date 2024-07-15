8272239fc795674837528e0c3810e2f6c67f26b7
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2024-07-15 10:19:49 -0400

    feat(test_metric_reporter): add the test metric reporter script with circleci json parsing
    Closes #ECTEEN-101

--- scripts/circleci_scraper/main.py
+++ scripts/circleci_scraper/main.py
@@ -31,7 +31,7 @@ def main(config_file: str = "config.ini") -> None:
             else "Scraping all available data."
         )
         client = CircleCIClient(config.circleci_scraper_config)
-        scraper = CircleCIScraper(config.main_config, client)
+        scraper = CircleCIScraper(config.common_config, client)
         scraper.export_test_metadata_and_artifacts(
             config.circleci_scraper_config.pipelines, config.circleci_scraper_config.date_limit
         )
--- scripts/common/error.py
+++ scripts/common/error.py
@@ -2,19 +2,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-"""CircleCIScraper error object"""
+"""Custom error handling for the ecosystem test scripts."""
 
 
 class BaseError(Exception):
-    """Base Error for Scraper."""
+    """Base class for custom exceptions in the ecosystem test scripts."""
 
     def __init__(self, message: str, inner_exception: Exception | None = None) -> None:
-        """Initialize the Error.
+        """Initialize the BaseError.
 
         Args:
             message (str): The error message.
-            inner_exception (Exception | None): The inner exception that caused this error.
-                                                Defaults to None.
+            inner_exception (Exception | None): The inner exception that caused this error, if any.
         """
         super().__init__(message)
         self.inner_exception = inner_exception
@@ -23,7 +22,7 @@ class BaseError(Exception):
         """Return the string representation of the error.
 
         Returns:
-            str: The string representation of the error, including the inner exception if present.
+            str: The error message, including the inner exception if present.
         """
         if self.inner_exception:
             return f"{super().__str__()} (caused by {self.inner_exception})"
